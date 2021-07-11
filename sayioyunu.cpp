49e4ed8fe39b90b4da24e89d02057026b5ec7eda
Murat Engin Unal <munal@omp.com>
2021-07-12 01:11:12 +0200

    pseudo raslantisal sayi tuttur

--- sayioyunu.cpp
+++ sayioyunu.cpp
@@ -1,6 +1,8 @@
 #include <random>
 #include <array>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 namespace SayiOyunu
 {
@@ -8,14 +10,16 @@ namespace SayiOyunu
     {
         public:
             explicit Oynatici();
+
             void printSayi(std::ostream &output) const;
 
         private:
             constexpr static const auto N = 4;
-            
+            using BasamakTipi = unsigned short;
+
             void sayiTut();
             
-            std::array<unsigned short, N> sayi;
+            std::array<BasamakTipi, N> sayi;
     }
 }
 
@@ -37,11 +41,27 @@ Oynatici::Oynatici()
 void Oynatici::sayiTut()
 {
     auto generator = std::mt19937{0};
-    auto rand9 = std::uniform_int_distribution<unsigned short>{1, 9};
-    auto rand10 = std::uniform_int_distribution<unsigned short>{0, 9};
+    auto rand9 = std::uniform_int_distribution<BasamakTipi>{1, 9};
 
     sayi[0] = rand9(generator);
 
+    auto olasiBasamaklar = std::vector<BasamakTipi>{};
+
     for (int i = 1; i < N; i++)
-        sayi[i] = rand10(generator);
+    {        
+        assert(std::is_sorted(olasiBasamaklar.cbegin(), olasiBasamaklar.cend()));
+
+        auto randRest = std::uniform_int_distribution<BasamakTipi>{0, 10 - i};
+        auto olasiSiraNo = randRest(generator);
+
+        auto itOlasi = std::upper_bound(olasiBasamaklar.cbegin(), olasiBasamaklar.cend(), olasiSiraNo);
+        assert(itOlasi >= olasiBasamaklar.cbegin());
+
+        sayi[i] = olasiSiraNo + (itOlasi - olasiBasamaklar.cbegin());
+        assert(sayi[i] < 10);
+        assert(!std::binary_search(olasiBasamaklar.cbegin(), olasiBasamaklar.cend(), sayi[i]));
+
+        auto itCikanlar = std::upper_bound(olasiBasamaklar.cbegin(), olasiBasamaklar.cend(), olasiSiraNo);
+        olasiBasamaklar.insert(itCikanlar, sayi[i]);
+    }
 }
